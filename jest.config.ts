5ec38299f3970cf6ec783a572dcc4a8c5cac4117
VladSmirnou <smr.vladislav@gmail.com>
2024-08-25 13:24:45 +0300

    jest testing support for native js files

--- jest.config.ts
+++ jest.config.ts
@@ -1,9 +1,17 @@
 export default {
+    // js files import support:
+    preset: 'ts-jest/presets/js-with-ts',
+    // --------------------------
     testEnvironment: 'jsdom',
     transform: {
         '^.+\\.tsx?$': 'ts-jest',
+        // js files import support:
+        'node_modules/variables/.+\\.(j|t)sx?$': 'ts-jest',
+        // --------------------
     },
-
+    // js files import support:
+    transformIgnorePatterns: ['node_modules/(?!troublesome-dependency/.*)'],
+    // ----------------------
     moduleNameMapper: {
         '\\.(css|less|sass|scss)$': 'identity-obj-proxy',
         '^.+\\.svg$': 'jest-transformer-svg',
